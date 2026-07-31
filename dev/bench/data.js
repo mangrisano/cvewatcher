89cc23e47f50ae2fe06f71b1d24c30510abede12
github-action-benchmark <github@users.noreply.github.com>
2026-07-31 21:37:04 +0000

    add cvewatcher benchmarks (pytest) benchmark result for 5bf663a85c67a9cff2d3c0019a883a6ac85833b7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785533420976,
+  "lastUpdate": 1785533824464,
   "repoUrl": "https://github.com/mangrisano/cvewatcher",
   "entries": {
     "cvewatcher benchmarks": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000002589459615961606",
             "extra": "mean: 52.86448862129794 usec\nrounds: 11293"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michele.angrisano@gmail.com",
+            "name": "Michele Angrisano",
+            "username": "mangrisano"
+          },
+          "committer": {
+            "email": "michele.angrisano@gmail.com",
+            "name": "Michele Angrisano",
+            "username": "mangrisano"
+          },
+          "distinct": true,
+          "id": "5bf663a85c67a9cff2d3c0019a883a6ac85833b7",
+          "message": "ci: pin ruff lint rule set to keep CI stable across ruff versions",
+          "timestamp": "2026-07-31T23:36:27+02:00",
+          "tree_id": "d9e3c119464b4628708ea50ddaffba35a870e5dc",
+          "url": "https://github.com/mangrisano/cvewatcher/commit/5bf663a85c67a9cff2d3c0019a883a6ac85833b7"
+        },
+        "date": 1785533823746,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/bench_perf.py::test_cpe_matches_name",
+            "value": 98013.4271369013,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000010353722597440356",
+            "extra": "mean: 10.202683746618096 usec\nrounds: 20648"
+          },
+          {
+            "name": "benchmarks/bench_perf.py::test_version_affected",
+            "value": 53954.44028452483,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000005593946755537745",
+            "extra": "mean: 18.534155756719418 usec\nrounds: 16789"
+          },
+          {
+            "name": "benchmarks/bench_perf.py::test_match_pipeline",
+            "value": 21904.15090898469,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000002720462407976836",
+            "extra": "mean: 45.653447337683275 usec\nrounds: 12865"
           }
         ]
       }
